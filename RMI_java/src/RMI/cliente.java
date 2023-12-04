31dff3606040ea10f22d7ccf8c5c1ef194d5e412
J-Pinos3 <josloco665@gmail.com>
2023-12-04 16:39:57 -0500

    Menu

--- RMI_java/src/RMI/cliente.java
+++ RMI_java/src/RMI/cliente.java
@@ -18,13 +18,67 @@ public class cliente {
         interfaz objeto = (interfaz) registro.lookup("ClienteRemoto");
         System.out.println( objeto.mensaje() + "\n" );
 
-        double a =  Double.parseDouble( JOptionPane.showInputDialog("Ingrese un número a: ") );
-        double b =  Double.parseDouble( JOptionPane.showInputDialog("Ingrese un número b: ") );
+        int opcion = 1;
+        double a = 1;
+        double b = 1;
 
-        System.out.printf( "%f + %f = %f %n",a,b,objeto.suma(a,b) );
-        System.out.printf( "%f - %f = %f %n",a,b,objeto.resta(a,b) );
-        System.out.printf( "%f * %f = %f %n",a,b,objeto.multiplicar(a,b) );
-        System.out.printf( "%f / %f = %f",a,b,objeto.dividir(a,b) );
+        while(opcion != 0){
+                opcion = Menu();
+            a =  Double.parseDouble( JOptionPane.showInputDialog("Ingrese un número a: ") );
+            b =  Double.parseDouble( JOptionPane.showInputDialog("Ingrese un número b: ") );
+            switch (opcion){
+                case 1:{
+                    JOptionPane.showMessageDialog(null, a + " + " + b + " = " + objeto.suma(a,b));
+                }
+                break;
+
+                case 2:{
+                    JOptionPane.showMessageDialog(null, a + " - " + b + " = " + objeto.resta(a,b));
+                }
+                break;
+
+
+                case 3:{
+                    JOptionPane.showMessageDialog(null, a + " * " + b + " = " + objeto.multiplicar(a,b));
+                }
+                break;
+
+
+                case 4:{
+                    JOptionPane.showMessageDialog(null, a + " / " + b + " = " + objeto.dividir(a,b));
+                }
+                break;
+
+                case 0:{
+                    JOptionPane.showMessageDialog(null, "Gracias por su preferencia");
+                }
+                break;
+
+                default:{
+                    JOptionPane.showMessageDialog(null, "Esa opción no existe");
+                }
+                break;
+            }
+        }
+
+
+
+
+
+
 
     }
+
+    public static int Menu(){
+        int opcion = Integer.parseInt(  JOptionPane.showInputDialog("1) Sumar\n"+
+                "2) Restar\n"+
+                "3) Multiplicar\n"+
+                "4) Dividir\n"+
+                "0) Salir") );
+
+
+        return opcion;
+    }
+
+
 }
